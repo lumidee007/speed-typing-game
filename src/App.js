cd025a0277d7cb1442dd9560d1e1785f8dbf5e04
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2021-10-09 11:33:58 +0100

    added-handleChange-function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import { useState } from "react"
 
 export default function App() {
+
+  const [text, setText] = useState(9)
+
+  const handleChange = (e) => {
+    setText(e.target.value)
+  }
+
   return (
     <div>
       <h1>Speed typing game</h1>
-      <textarea/>
+      <textarea
+      value={text}
+      onChange={handleChange}
+      />
             <h4>Time remaining: ???</h4>
             <button >
                 Start
             </button>
-            <h2>Word count: ???</h2>
+            <h2>Word count : {text}</h2>
 
     </div>
   )
