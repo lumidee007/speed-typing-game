b36f439e018ad1257f4e565064b67c1bf9375264
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2021-10-09 11:26:41 +0100

    added-styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
 export default function App() {
   return (
-    <div className="app">
+    <div>
       <h1>Speed typing game</h1>
-
+      <textarea/>
+            <h4>Time remaining: ???</h4>
+            <button >
+                Start
+            </button>
+            <h2>Word count: ???</h2>
 
     </div>
   )
